0f68d0ff614a1575e39ce25fe2d11b9b7b8ea61d
bispojr <esdraspiano@gmail.com>
2013-12-12 00:50:15 -0200

    Modificando a classe MaquinaTuring
    - Adicionando métodos à classe MaquinaTuring
    OBS: Alguns métodos e atributos ainda precisam ser implementados.

--- src/br/org/freeUFG/teoComp2013/MaquinaTuring.java
+++ src/br/org/freeUFG/teoComp2013/MaquinaTuring.java
@@ -1,24 +1,67 @@
 /*
     Copyright © 2013 Esdras Bispo Jr.
    
-    This file is part of BWEvaluator.
+    This file is part of BonusTeoComp.
 
-    BWEvaluator is free software: you can redistribute it and/or modify
+    BonusTeoComp is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    BWEvaluator is distributed in the hope that it will be useful,
+    BonusTeoComp is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with BWEvaluator.  If not, see <http://www.gnu.org/licenses/>.
+    along with BonusTeoComp.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package br.org.freeUFG.teoComp2013;
 
+import java.util.ArrayList;
+
 public class MaquinaTuring {
+    
+    private ArrayList<Character> fita;
+    private ArrayList<String> delta;    //Pensar na função delta
+    private int cabeca;
+    
+    //Pensar no construtor
+    public MaquinaTuring(){
+        
+    }
+    //Necessário implementar este método
+    public String rodar(String cadeia){
+        this.recebeEntrada(cadeia);
+        return "aceita";
+    }
+    private void recebeEntrada(String w){
+        this.cabeca = 0;
+        this.fita = new ArrayList<> ();  
+        for(int i=0; i<w.length(); i++){
+            Character c = new Character(w.charAt(i));
+            this.fita.add(c);
+        }
+    }
+    private void moverCabeça(String direcao){
+        if(direcao.equals("direita")){
+            this.cabeca++;
+            //Caso em que se move para uma célula em branco
+            if(this.cabeca >= this.fita.size()){    
+                Character c = new Character('_');
+                this.fita.add(c);
+            }
+        }
+        if(direcao.equals("esquerda")){
+            if(this.cabeca > 0){
+                this.cabeca--;
+            }
+        }
+    }
+    private void escreveNaFita(char s){
+        Character c = new Character(s);
+        this.fita.set(this.cabeca, c);
+    }
     
 }
